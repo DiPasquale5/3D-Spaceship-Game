d27b395410b8c8756c51749f01d557a2bfd08ebf
Domenic DiPasquale <Home@Domenics-MacBook-Pro.local>
2013-12-16 15:54:06 -0500

    Initial Commit

---
+++ ObjectReader/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ObjectReader
+//
+//  Created by Domenic DiPasquale on 12/16/2013.
+//  Copyright (c) 2013 Domenic DiPasquale. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
